da4a388bb6bdd745b680a3848b761a8991db62d5
9salman7 <salman97@gmail.com>
2020-03-05 22:33:23 +0530

    ..

--- neural networks/rc_driver_keras2.py
+++ neural networks/rc_driver_keras2.py
@@ -38,7 +38,7 @@ class RCControl(object):
 				sep = ' '
 				buf = ''
 				while sep not in buf:
-					buf+=connection.recv(8)
+					buf+=self.connection.recv(8)
 				prediction=int(buf)
 				
 				if prediction == 2:
@@ -89,7 +89,7 @@ class RCControl(object):
 					GPIO.output(7, GPIO.LOW) # Turn on
 					GPIO.output(4, GPIO.HIGH)
 		finally:
-			#self.connection.close()
+			self.connection.close()
 			self.server_socket.close()
 
 
