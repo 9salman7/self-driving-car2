cac552941bc0cb425582e05fe316f85d157ba3db
9salman7 <salman97@gmail.com>
2020-03-05 15:33:01 +0530

    ..

--- neural networks/rc_driver_nn_only.py
+++ neural networks/rc_driver_nn_only.py
@@ -41,6 +41,7 @@ class RCDriverNNOnly(object):
                     # lower half of the image
                     height, width = gray.shape
                     roi = grayroi = gray[int(height/2):height, :]
+                    print("image loaded")
 
                     cv2.imshow('image', image)
                     # cv2.imshow('mlp_image', roi)
