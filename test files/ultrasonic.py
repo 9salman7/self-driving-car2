29b128df3dc45e0707d9c942aa696150e608123e
Rudresh <47209075+rudreshm@users.noreply.github.com>
2020-04-05 23:30:47 +0530

    Update ultrasonic.py

--- test files/ultrasonic.py
+++ test files/ultrasonic.py
@@ -1,37 +1,37 @@
 import RPi.GPIO as GPIO
 import time
+while True:
+    try:
+          GPIO.setmode(GPIO.BCM)
 
-try:
-      GPIO.setmode(GPIO.BCM)
+          PIN_TRIGGER = 21
+          PIN_ECHO = 20
 
-      PIN_TRIGGER = 21
-      PIN_ECHO = 20
+          GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+          GPIO.setup(PIN_ECHO, GPIO.IN)
 
-      GPIO.setup(PIN_TRIGGER, GPIO.OUT)
-      GPIO.setup(PIN_ECHO, GPIO.IN)
+          GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
-      GPIO.output(PIN_TRIGGER, GPIO.LOW)
+          print "Waiting for sensor to settle"
 
-      print "Waiting for sensor to settle"
+          time.sleep(2)
 
-      time.sleep(2)
+          print "Calculating distance"
 
-      print "Calculating distance"
+          GPIO.output(PIN_TRIGGER, GPIO.HIGH)
 
-      GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+          time.sleep(0.00001)
 
-      time.sleep(0.00001)
+          GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
-      GPIO.output(PIN_TRIGGER, GPIO.LOW)
+          while GPIO.input(PIN_ECHO)==0:
+                pulse_start_time = time.time()
+          while GPIO.input(PIN_ECHO)==1:
+                pulse_end_time = time.time()
 
-      while GPIO.input(PIN_ECHO)==0:
-            pulse_start_time = time.time()
-      while GPIO.input(PIN_ECHO)==1:
-            pulse_end_time = time.time()
+          pulse_duration = pulse_end_time - pulse_start_time
+          distance = round(pulse_duration * 17150, 2)
+          print "Distance:",distance,"cm"
 
-      pulse_duration = pulse_end_time - pulse_start_time
-      distance = round(pulse_duration * 17150, 2)
-      print "Distance:",distance,"cm"
-
-finally:
-      GPIO.cleanup()
+    finally:
+          GPIO.cleanup()
