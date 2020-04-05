f669d5e397cd610a129426a7c1e5c8646684a888
Rudresh <47209075+rudreshm@users.noreply.github.com>
2020-04-05 22:17:54 +0530

    Update ultrasonic_client.py

--- test files/ultrasonic_client.py
+++ test files/ultrasonic_client.py
@@ -8,7 +8,7 @@ GPIO.setwarnings(False)
 
 # create a socket and bind socket to the host
 client_socket = socket(AF_INET, SOCK_STREAM)
-client_socket.connect(('0.0.0.0', 5678))   #laptop
+client_socket.connect(('192.168.0.102', 5678))   #laptop
 
 def measure():
     """
