2789551dccbda011cd3c7549c9f165f0f83d26c6
Rudresh <47209075+rudreshm@users.noreply.github.com>
2020-04-05 23:10:33 +0530

    Update ultrasonic.py

--- test files/ultrasonic.py
+++ test files/ultrasonic.py
@@ -1,53 +1,49 @@
-#Libraries
 import RPi.GPIO as GPIO
 import time
- 
-#GPIO Mode (BOARD / BCM)
-GPIO.setmode(GPIO.BCM)
- 
-#set GPIO Pins
-GPIO_TRIGGER = 21
-GPIO_ECHO = 20
- 
-#set GPIO direction (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
- 
-def distance():
-    # set Trigger to HIGH
 
-    GPIO.output(GPIO_TRIGGER, GPIO.HIGH)
- 
-    # set Trigger after 0.01ms to LOW
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, GPIO.LOW)
-    StartTime = time.time()
-    StopTime = time.time()
- 
-    # save StartTime
-    while GPIO.input(GPIO_ECHO) == 0:
-        StartTime = time.time()
- 
-    # save time of arrival
-    while GPIO.input(GPIO_ECHO) == 1:
-        StopTime = time.time()
- 
-    # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
- 
-    return distance
- 
-if __name__ == '__main__':
-    try:
-        while True:
-            dist = distance()
-            print ("Measured Distance = %.1f cm" % dist)
-            time.sleep(1)
- 
-        # Reset by pressing CTRL + C
-    except KeyboardInterrupt:
-        print("Measurement stopped by User")
-        GPIO.cleanup()
+
+GPIO.setmode(GPIO.BCM)
+
+TRIG = 21
+ECHO = 20
+i=0
+
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+
+GPIO.output(TRIG, False)
+print "Calibrating....."
+time.sleep(2)
+
+print "Place the object......"
+
+
+try:
+    while True:
+       GPIO.output(TRIG, True)
+       time.sleep(0.00001)
+       GPIO.output(TRIG, False)
+
+       while GPIO.input(ECHO)==0:
+          pulse_start = time.time()
+
+       while GPIO.input(ECHO)==1:
+          pulse_end = time.time()
+
+       pulse_duration = pulse_end - pulse_start
+
+       distance = pulse_duration * 17150
+
+       distance = round(distance+1.15, 2)
+  
+       if distance<=20 and distance>=5:
+          print "distance:",distance,"cm"
+          i=1
+          
+       if distance>20 and i==1:
+          print "place the object...."
+          i=0
+       time.sleep(2)
+
+except KeyboardInterrupt:
+     GPIO.cleanup()
